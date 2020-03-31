571f7522b59d255d7d8075b821b4ec607ef93c51
Rajko Vukovic <rajkovukovic@gmail.com>
2020-03-31 13:11:24 +0200

    fix jspm bin path resolving when running from global scope

--- utils/constants.js
+++ utils/constants.js
@@ -4,8 +4,12 @@ const os = require("os");
 const path = require("path");
 const untildify = require("untildify");
 
+const NODE_MODULES_PATH = path.join(
+  require.resolve("jspm").split("/node_modules/")[0],
+  "node_modules"
+);
+const JSPM_BIN_PATH = path.join(NODE_MODULES_PATH, ".bin/jspm");
 const CWD = process.cwd();
-const JSPM_BIN_PATH = path.join(CWD, "node_modules/.bin/jspm");
 const TMP_DIR = path.resolve(untildify(path.join(os.tmpdir(), "vatra")));
 const VATRA_PATH_ENV = process.env.VATRA_PATH;
 const VATRA_PATH = path.resolve(
@@ -15,8 +19,8 @@ const VATRA_LIB_NAME = "lib";
 const VATRA_LIB_PATH = path.join(VATRA_PATH, VATRA_LIB_NAME);
 const JS_EXTENSIONS = [".js", ".mjs"];
 
-exports.CWD = CWD;
 exports.JSPM_BIN_PATH = JSPM_BIN_PATH;
+exports.CWD = CWD;
 exports.TMP_DIR = TMP_DIR;
 exports.VATRA_PATH = VATRA_PATH;
 exports.VATRA_LIB_NAME = VATRA_LIB_NAME;
