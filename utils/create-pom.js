1c04685d1ada17543dd851ccc59fea617f406fe7
Ivan <ivan.antonijevic@braintribe.com>
2020-04-06 17:28:44 +0200

    Move to utils, tests

--- utils/create-pom.js
+++ utils/create-pom.js
@@ -1,73 +1,30 @@
 "use strict";
 
 const fs = require('fs');
-const { getPomVersionFormat } = require("./versions/versions");
-const { getPomRange } = require("./versions/versions");
+const { getPom } = require("./versions/versions");
 
 console.log('### Started Readin package-lock');
 
 const packLock = require('../package-lock.json');
+const pack = require('../package.json');
 
 console.log('### package-lock: ', packLock.name, packLock.version);
 
-let txt = '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">\n' +
-    '\t<modelVersion>4.0.0</modelVersion>\n' +
-    `\t<groupId>tribefire.cortex.ux</groupId>\n` +
-    `\t<artifactId>${packLock.name}</artifactId>\n` +
-    `\t<version>${packLock.version}</version>\n` +
-    '\t<packaging>pom</packaging>\n' +
-    `\t<name>${packLock.name} - ${packLock.version}</name>\n` +
-    '\t<dependencyManagement>\n' +
-    '\t\t<dependencies>\n';
-
 let requiresMap = {};
+
+Object.entries(pack.dependencies).forEach(d => {
+    requiresMap[d[0]] = { name: d[0], versionRanges: [d[1]], resolvedVersion: packLock.dependencies[d[0]] ? packLock.dependencies[d[0]].version : '' };
+});
+
 Object.entries(packLock.dependencies).filter(dep => !dep[1].dev && dep[1].requires)
     .forEach(d => {
         Object.entries(d[1].requires).forEach(
             r => {
-                requiresMap[r[0]] = requiresMap[r[0]] ? [...requiresMap[r[0]], getPomRange(r[1], '')] : [getPomRange(r[1], '')];
+                requiresMap[r[0]] = requiresMap[r[0]] ? { ...requiresMap[r[0]], versionRanges: [...requiresMap[r[0]].versionRanges, r[1]] } : { name: r[0], versionRanges: [r[1]], resolvedVersion: packLock.dependencies[r[0]].version };;
             }
         )
     });
-
-Object.entries(packLock.dependencies)
-    .filter(dep => !dep[1].dev)
-    .forEach((k) => {
-        const dep = getGroupAndArt(k[0]);
-        const ranges = getVersion(k);
-        ranges.forEach(rang => {
-            txt += '\t\t<dependency>\n' +
-                `\t\t\t<groupId>${dep.group}</groupId>\n` +
-                `\t\t\t<artifactId>${dep.artifact}</artifactId>\n` +
-                `\t\t\t<version>${rang}</version>\n` +
-                '\t\t</dependency>\n';
-        })
-    })
-
-txt += '\t\t</dependencies> \n\t\ </dependencyManagement> \n </project>',
-
-    writePom('pom.xml', txt);
-
-function getGroupAndArt(name) {
-    const parts = name.split('/')
-    if (parts.length > 1) {
-        return {
-            group: parts.splice(0, parts.length - 1).join('.'),
-            artifact: parts[parts.length - 1]
-        }
-    } else {
-        return {
-            group: parts[0],
-            artifact: parts[0]
-        }
-    }
-}
-function getVersion(dep) {
-    if (requiresMap[dep[0]]) {
-        return getPomVersionFormat(requiresMap[dep[0]], dep[1].version);
-    }
-    return [dep[1].version];
-}
+writePom('pom.xml', getPom(`test.pom.parser:${pack.name}#${pack.version}`, Object.values(requiresMap)));
 
 function writePom(name, txt) {
     fs.writeFile(
--- utils/versions/versions.js
+++ utils/versions/versions.js
@@ -1,6 +1,16 @@
 "use strict";
+const Constants = require('../constants');
+
 const semver = require('semver');
 
+
+/**
+ * 
+ * @param {[{min: string, max: string}]} arr 
+ * @param {string} resolved version
+ * 
+ * @returns {string[]} eg. ['[1.2.7, 1.3.0)', '[1.3.5, 1.4.3)']
+ */
 function getPomVersionFormat(arr, resolved) {
     if (arr.length == 1) return [`[${arr[0].min}, ${arr[0].max})`];
     let range = {}
@@ -24,6 +34,13 @@ function getPomVersionFormat(arr, resolved) {
     return resp;
 }
 
+/**
+ * 
+ * @param {string} range eg. ^6.2.3, *, 1.2.*, ~1.2.3 ... (see semver)
+ * @param {string} resolved version
+ * 
+ * @returns {{min: string, max: string}} eg. {min: 6.2.3, max: 6.3.0}
+ */
 function getPomRange(range, resolved) {
     // If exist =, or v or concrete version, then that version should be used, or new dependency added if version not mach some range
     // if || exist  - can  be treated as  new range - fixed in create range
@@ -134,5 +151,64 @@ function normalizeVersion(version) {
             : version + '.0.0';
 }
 
+/**
+ * 
+ * @param {[{ name, versionRanges, resolvedVersion }]} depMap 
+ * 
+ * @return {[string]} - xml dependency
+ */
+function getDependencies(depMap) {
+    const resp = [];
+    depMap.forEach(dep => {
+        const artifact = getGroupAndArt(dep.name);
+        const ranges = getPomVersionFormat(dep.versionRanges.map(r => getPomRange(r, dep.resolvedVersion)), dep.resolvedVersion);
+        ranges.forEach(r => {
+            resp.push('<dependency>\n' +
+                `\t<groupId>${artifact.group}</groupId>\n` +
+                `\t<artifactId>${artifact.artifact}</artifactId>\n` +
+                `\t<version>${r}</version>\n` +
+                '</dependency>');
+        })
+    });
+    return resp;
+}
+
+function getGroupAndArt(name) {
+    const parts = name.split('/')
+    if (parts.length > 1) {
+        return {
+            group: parts.splice(0, parts.length - 1).join(Constants.GROUP_SEPARATOR).replace('@', ''),
+            artifact: parts[parts.length - 1]
+        }
+    } else {
+        return {
+            group: Constants.DEFAULT_GROUP,
+            artifact: parts[0]
+        }
+    }
+}
+
+function getPom(artifact, depMap) {
+    const tmp = artifact.replace('#', ':').split(':');
+    let txt = '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">\n' +
+        '\t<modelVersion>4.0.0</modelVersion>\n' +
+        `\t<groupId>${tmp[0]}</groupId>\n` +
+        `\t<artifactId>${tmp.length > 1 ? tmp[1] : ''}</artifactId>\n` +
+        `\t<version>${tmp.length > 2 ? tmp[2] : ''}</version>\n` +
+        '\t<packaging>pom</packaging>\n' +
+        `\t<name>${artifact}</name>\n` +
+        '\t<dependencies>\n';
+
+    getDependencies(depMap).forEach(dep => {
+        txt += dep;
+    })
+
+    txt += '\t</dependencies> \n </project>';
+    return txt;
+
+}
+
 exports.getPomVersionFormat = getPomVersionFormat;
 exports.getPomRange = getPomRange;
+exports.getDependencies = getDependencies;
+exports.getPom = getPom;
