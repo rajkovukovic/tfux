95219045eec6014e9e479bff00c64df0b01f11f5
Rajko Vukovic <rajkovukovic@gmail.com>
2020-04-23 15:46:30 +0200

    more tests

--- utils/engines/jspm/install/install-dependencies-with-peer.js
+++ utils/engines/jspm/install/install-dependencies-with-peer.js
@@ -1,48 +1,37 @@
-"use strict";
+'use strict';
 
-const path = require("path");
-const childProcess = require("child_process");
-const { JSPM_BIN_PATH } = require("../../../constants/constants.js");
-const { parseJspmJSONDependency } = require("../tools/parse-jspm.js");
+const path = require('path');
+const childProcess = require('child_process');
+const { JSPM_BIN_PATH } = require('../../../constants/constants.js');
+const { parseJspmJSONDependency } = require('../tools/parse-jspm.js');
 
 function jspmFormatDependencies(dependencyMap) {
   return Object.entries(dependencyMap)
-    .filter(([moduleName]) => moduleName !== "@jspm/core")
+    .filter(([moduleName, _]) => moduleName !== '@jspm/core')
     .map(([_, moduleJspmName]) => moduleJspmName);
 }
 
 function packageJsonFormatDependencies(dependencyMap) {
   return Object.entries(dependencyMap)
-    .filter(([moduleName]) => moduleName !== "@jspm/core")
+    .filter(([moduleName]) => moduleName !== '@jspm/core')
     .map(([moduleName, moduleVersion]) => `${moduleName}@${moduleVersion}`);
 }
 
-function installPeerDependenciesIfAny(
-  engine,
-  moduleJspmName,
-  checkedDependencies
-) {
+function installPeerDependenciesIfAny(engine, moduleJspmName, checkedDependencies) {
   if (!checkedDependencies.has(moduleJspmName)) {
-    console.log("installPeerDependenciesIfAny", moduleJspmName);
+    console.log('installPeerDependenciesIfAny', moduleJspmName);
     checkedDependencies.add(moduleJspmName);
 
     const packageJson = require(path.join(
       engine.installedModulesPath,
-      moduleJspmName.split(":").join("/"),
-      "package.json"
+      moduleJspmName.split(':').join('/'),
+      'package.json'
     ));
 
-    const peerDependencies = packageJsonFormatDependencies(
-      packageJson.peerDependencies
-    );
+    const peerDependencies = packageJsonFormatDependencies(packageJson.peerDependencies);
 
     if (peerDependencies.length > 0) {
-      installDependenciesWithPeer(
-        engine,
-        peerDependencies,
-        true,
-        checkedDependencies
-      );
+      installDependenciesWithPeer(engine, peerDependencies, true, checkedDependencies);
       const { jspmJSON } = engine;
       // const parentName = parseJspmJSONDependency(moduleJspmName)[1];
       peerDependencies.forEach((peerDep) => {
@@ -51,8 +40,7 @@ function installPeerDependenciesIfAny(
         console.log(
           `\n\n\nWriting: jspmJSON.dependencies.${moduleJspmName}.resolve.${peerDepName} = jspmJSON.resolve.${peerDepName} (${jspmJSON.resolve[peerDepName]})\n\n\n`
         );
-        jspmJSON.dependencies[moduleJspmName].resolve[peerDepName] =
-          jspmJSON.resolve[peerDepName];
+        jspmJSON.dependencies[moduleJspmName].resolve[peerDepName] = jspmJSON.resolve[peerDepName];
       });
       engine.saveJspmJSON();
     }
@@ -86,11 +74,9 @@ function installDependenciesWithPeer(
   checkedDependencies
 ) {
   childProcess.execSync(
-    `cd ${
-      engine._installedModulesRootPath
-    } && ${JSPM_BIN_PATH} install ${dependencies.join(" ")}`,
+    `cd ${engine._installedModulesRootPath} && ${JSPM_BIN_PATH} install ${dependencies.join(' ')}`,
     {
-      stdio: "inherit",
+      stdio: 'inherit',
     }
   );
 
@@ -106,4 +92,5 @@ function installDependenciesWithPeer(
   }
 }
 
+exports.jspmFormatDependencies = jspmFormatDependencies;
 exports.installDependenciesWithPeer = installDependenciesWithPeer;
